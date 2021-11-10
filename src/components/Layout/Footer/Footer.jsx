b30ac7c76ef0db6132bc4095f0ff6c0739e6bc39
Tyler <tythedeveloper10@gmail.com>
2021-11-10 06:19:07 -0600

    add header and footer components

---
+++ src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import footerStyles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={footerStyles.footer}>
+      <span>Built by Tyler Ridings</span>
+    </footer>
+  );
+};
+
+export default Footer;
